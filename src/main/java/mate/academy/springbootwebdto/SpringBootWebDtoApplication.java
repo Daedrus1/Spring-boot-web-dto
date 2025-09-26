1ca09b180f53a16d0b949aa7e1133c7c416c30db
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2025-09-26 17:13:46 +0200

    Initial commit: project + checkstyle

---
+++ src/main/java/mate/academy/springbootwebdto/SpringBootWebDtoApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.springbootwebdto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebDtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebDtoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mate/academy/springbootwebdto/SpringBootWebDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.springbootwebdto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebDtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
