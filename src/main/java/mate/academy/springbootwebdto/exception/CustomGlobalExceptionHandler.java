2b418aea81454c39376ca192a90ed86297901f3f
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2025-10-14 18:30:35 +0200

    Add Pagination, Sorting, and Swagger to the existing controllers.

--- src/main/java/mate/academy/springbootwebdto/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/mate/academy/springbootwebdto/exception/CustomGlobalExceptionHandler.java
@@ -26,7 +26,6 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
     ) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.BAD_REQUEST);
         List<String> errors = ex.getBindingResult().getAllErrors().stream()
                 .map(this::getErrorsMassage)
                 .toList();
@@ -43,4 +42,5 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         }
         return e.getDefaultMessage();
     }
+
 }
