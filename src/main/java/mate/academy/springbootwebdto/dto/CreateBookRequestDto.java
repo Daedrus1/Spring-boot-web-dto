4b1b784cb99417f5c6d545455f4b52e63115d664
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2025-10-17 12:05:54 +0200

    Exception and validation

--- src/main/java/mate/academy/springbootwebdto/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/springbootwebdto/dto/CreateBookRequestDto.java
@@ -11,15 +11,12 @@ import org.hibernate.validator.constraints.URL;
 
 @Data
 public class CreateBookRequestDto {
-    @NotBlank(message = "Title is required")
     @Size(min = 1, max = 200, message = "Title must be 1–200 characters")
     private String title;
 
-    @NotBlank(message = "Author is required")
     @Size(min = 2, max = 120, message = "Author must be 2–120 characters")
     private String author;
 
-    @NotBlank(message = "ISBN is required")
     private String isbn;
 
     @NotNull(message = "Price is required")
