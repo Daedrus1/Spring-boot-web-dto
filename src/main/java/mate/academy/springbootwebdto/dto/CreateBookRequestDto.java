3a3fdf4a4b0b5876fb880054801c5a99778d27bd
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2025-10-17 12:08:57 +0200

    Exception and validation

--- src/main/java/mate/academy/springbootwebdto/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/springbootwebdto/dto/CreateBookRequestDto.java
@@ -2,7 +2,6 @@ package mate.academy.springbootwebdto.dto;
 
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
